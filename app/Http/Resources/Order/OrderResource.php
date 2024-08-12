38579c0306ab55d190e7fe2076d24919b5339a49
Andrew Dobrovolsky <117436049+adobrovolskybinariks@users.noreply.github.com>
2024-08-12 16:16:19 +0300

    Updates

--- app/Http/Resources/Order/OrderResource.php
+++ app/Http/Resources/Order/OrderResource.php
@@ -21,22 +21,24 @@ class OrderResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'               => $this->id,
-            'surname'          => $this->surname,
-            'name'             => $this->name,
-            'patronymic'       => $this->patronymic,
-            'phone'            => $this->phone,
-            'delivery_type'    => $this->delivery_type,
-            'comment'          => $this->comment,
-            'city_name'        => $this->city_name,
-            'warehouse_name'   => $this->warehouse_name,
-            'total_price_usd'  => $this->total_price_usd,
-            'total_price_uah'  => $this->total_price_uah,
-            'discount_percent' => $this->discount_percent,
-            'discount_usd'     => $this->discount_usd,
-            'discount_uah'     => $this->discount_uah,
-            'created_at'       => $this->created_at->format('d.m.Y H:i:s'),
-            'products'         => OrderProductResource::collection($this->products)
+            'id'                      => $this->id,
+            'surname'                 => $this->surname,
+            'name'                    => $this->name,
+            'patronymic'              => $this->patronymic,
+            'phone'                   => $this->phone,
+            'delivery_type'           => $this->delivery_type,
+            'comment'                 => $this->comment,
+            'city_name'               => $this->city_name,
+            'warehouse_name'          => $this->warehouse_name,
+            'total_price_usd'         => $this->total_price_usd,
+            'total_price_uah'         => $this->total_price_uah,
+            'discount_percent'        => $this->discount_percent,
+            'discount_usd'            => $this->discount_usd,
+            'discount_uah'            => $this->discount_uah,
+            'total_with_discount_uah' => round($this->total_price_uah - $this->discount_uah),
+            'total_with_discount_usd' => round($this->total_price_usd - $this->discount_usd, 2),
+            'created_at'              => $this->created_at->format('d.m.Y H:i:s'),
+            'products'                => OrderProductResource::collection($this->products)
         ];
     }
 }
