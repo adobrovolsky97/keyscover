e779ee8d15b1a6d49b2273e0779d7d39ddad6d44
Andrew Dobrovolsky <117436049+adobrovolskybinariks@users.noreply.github.com>
2024-08-19 12:12:10 +0300

    Updates

--- app/Jobs/ExportJob.php
+++ app/Jobs/ExportJob.php
@@ -3,7 +3,6 @@
 namespace App\Jobs;
 
 use App\Enums\Export\Status;
-use App\Enums\Product\Media;
 use App\Exports\User\UserExport;
 use App\Models\Export\Export;
 use Illuminate\Bus\Queueable;
@@ -12,6 +11,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Maatwebsite\Excel\Facades\Excel;
+use Storage;
 use Throwable;
 
 class ExportJob implements ShouldQueue
@@ -38,7 +38,12 @@ class ExportJob implements ShouldQueue
     public function handle(): void
     {
         try {
-            $fileName = $this->export->name. '_' . now()->format('Y-m-d H:i:s') . '.xlsx';
+            $fileName = $this->export->name . '_' . now()->format('Y-m-d H:i:s') . '.xlsx';
+
+            if (!Storage::disk('public')->exists('exports')) {
+                Storage::disk('public')->makeDirectory('exports');
+            }
+
             Excel::store(new UserExport(), "exports/$fileName", 'public');
 
             $this->export->update(['status' => Status::COMPLETED, 'file_path' => "exports/$fileName"]);
