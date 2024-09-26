1ad87d23e6abefd993f50491e687458a60258886
Andrew Dobrovolsky <117436049+adobrovolskybinariks@users.noreply.github.com>
2024-09-26 16:12:49 +0300

    updates

--- resources/js/views/product/Item.vue
+++ resources/js/views/product/Item.vue
@@ -5,6 +5,13 @@
                       :show-arrows="showArrows"
                       :media="product.media.length ? product.media : [{id: null, url: product.image}]"/>
         </figure>
+
+        <svg v-if="product.is_hidden" class="h-24 w-24 absolute z-[100] m-auto left-0 right-0 top-0 bottom-0" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+             stroke-linecap="round" stroke-linejoin="round">
+            <path
+                d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"/>
+            <line x1="1" y1="1" x2="23" y2="23"/>
+        </svg>
         <div class="card-body p-2 lg:p-4">
 
             <p v-if="!minified" class="text-xs text-gray-400">Арт. {{ product.sku }}</p>
@@ -21,8 +28,12 @@
             </router-link>
 
             <div class="flex flex-row justify-between items-center">
-                <div class="badge badge-outline badge-neutral badge-sm lg:badge-md" :class="{'!badge-sm': minified}">{{ product.usd_price }}$</div>
-                <div class="badge badge-outline badge-neutral badge-sm lg:badge-md" :class="{'!badge-sm': minified}">{{ product.price }} грн.</div>
+                <div class="badge badge-outline badge-neutral badge-sm lg:badge-md" :class="{'!badge-sm': minified}">
+                    {{ product.usd_price }}$
+                </div>
+                <div class="badge badge-outline badge-neutral badge-sm lg:badge-md" :class="{'!badge-sm': minified}">
+                    {{ product.price }} грн.
+                </div>
             </div>
             <div v-if="$store.state.user !== null" class="card-actions justify-center">
 
@@ -42,7 +53,9 @@
                             </button>
                         </div>
                     </div>
-                    <span class="text-error text-xs absolute -bottom-4" v-if="product.left_in_stock <= cartQty">В наявності: {{product.left_in_stock}} шт.</span>
+                    <span class="text-error text-xs absolute -bottom-4" v-if="product.left_in_stock <= cartQty">В наявності: {{
+                            product.left_in_stock
+                        }} шт.</span>
                 </div>
 
                 <button v-if="!cartProduct && product.left_in_stock > 0" @click="addItemToCart(product)"
