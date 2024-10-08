e177afab4e554d31562edcf5e24167dbb8aa4939
Andrew Dobrovolsky <117436049+adobrovolskybinariks@users.noreply.github.com>
2024-10-08 15:24:18 +0300

    Updated meta tags

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <title>{{$title}}</title>
+    <meta name="description" content="{{$description}}">
+
+    @if(!empty($meta))
+        @foreach($meta as $name => $content)
+           <meta property="og:{{$name}}" content="{{$content}}">
+        @endforeach
+    @endif
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="google-site-verification" content="VEovG_eeKiPLGsHM_vAGYayYTGZTA6aEeeR-GBIuRX0" />
+    <meta name="google-site-verification" content="VEovG_eeKiPLGsHM_vAGYayYTGZTA6aEeeR-GBIuRX0"/>
     <link rel="icon" href="{{asset('logo.png')}}">
     @vite('resources/js/app.js')
 </head>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Enums\Product\Media;
+use App\Models\Product\Product;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any}', function () {
-    return view('index');
+Route::get('/{any}', function (Request $request) {
+
+    $ogMeta = [];
+    $defaultDescription = 'Оптовий продаж автомобільних ключів та все що з ними повязано. Співпраця виключно з майстрами!';
+    $defaultTitle = 'KeysCover';
+
+    if ($request->is('product/*')) {
+        $productId = $request->segment(2);
+
+        $product = Product::query()->whereKey($productId)->first();
+
+        if (!empty($product)) {
+            $image = $product->getFirstMediaUrl(Media::COLLECTION_MAIN->value, 'watermarked');
+            $ogMeta = [
+                'title'       => $product->name,
+                'description' => $product->description ?? $defaultDescription,
+                'image'       => empty($image) ? asset('no-image.png') : $image,
+                'url'         => url()->current(),
+                'type'        => 'product',
+                'site_name'   => 'KeysCover',
+                'locale'      => 'ua_UA',
+            ];
+        }
+    }
+
+    return view('index', ['meta' => $ogMeta, 'title' => $defaultTitle, 'description' => $defaultDescription]);
 })->where('any', '.*');
