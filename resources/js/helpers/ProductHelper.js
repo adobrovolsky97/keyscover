e0319fdc9fb8cc60de4ae510f4354c839b3743f5
Andrew Dobrovolsky <117436049+adobrovolskybinariks@users.noreply.github.com>
2025-02-26 15:57:21 +0200

    Updates

--- resources/js/helpers/ProductHelper.js
+++ resources/js/helpers/ProductHelper.js
@@ -2,13 +2,13 @@ class CartHelper {
     getColorForStatus(status) {
         switch (status) {
             case 'Необроблені':
-                return 'text-gray-400';
+                return 'text-gray-400 text-light';
             case 'Успішно':
-                return 'text-black-400';
+                return 'text-black-400 text-light';
             case 'Потребує оплати':
                 return 'text-red-400 font-bold';
             default:
-                return 'text-green-400';
+                return 'text-green-400 text-light';
         }
     }
 }
