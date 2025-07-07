4c0daa1c6cb4899bd910eb7337bbc782a7e5362e
Andrew Dobrovolsky <117436049+adobrovolskybinariks@users.noreply.github.com>
2025-07-07 12:58:59 +0300

    Added MC

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,6 +14,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="google-site-verification" content="VEovG_eeKiPLGsHM_vAGYayYTGZTA6aEeeR-GBIuRX0"/>
     <link rel="icon" href="{{asset('logo.png')}}">
+
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "sb4da4yw92");
+    </script>
+
     @vite('resources/js/app.js')
 </head>
 <body>
